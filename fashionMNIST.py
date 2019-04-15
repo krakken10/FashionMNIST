85e87b17306602f17dc3998231a57328fc766163
krakken10 <49490036+krakken10@users.noreply.github.com>
2019-04-15 17:16:30 -0400

    Update fashionMNIST.py

--- fashionMNIST.py
+++ fashionMNIST.py
@@ -4,7 +4,6 @@ from keras.models import Sequential, load_model, save_model
 from keras.layers import Dense, Activation, MaxPooling2D, Flatten, Dropout, Conv2D
 from keras import backend as kerasBackend
 from keras.utils import np_utils
-#from util import get_normalized_data, y2indicator
 
 
 import loadData 
@@ -12,6 +11,7 @@ import plot
 
 train_data_path = "dataset/fashion-mnist_train.csv"
 test_data_path = "dataset/fashion-mnist_test.csv"
+
 # initialize the label names
 labelNames = ["top", "trouser", "pullover", "dress", "coat",
 	"sandal", "shirt", "sneaker", "bag", "ankle boot"]
@@ -29,7 +29,6 @@ print("image_width = ",image_width )
 
 
 #load training data
-
 load_data = loadData.LoadData(image_size)
 
 train_data = pd.read_csv(train_data_path)
@@ -63,25 +62,13 @@ x_test = data_normalize(x_test, 255.0)
 y_train = np_utils.to_categorical(y_train, num_classes)
 y_test = np_utils.to_categorical(y_test, num_classes)
 
-
-
-
-
 model = Sequential()
-# input: 100x100 images with 3 channels -> (100, 100, 3) tensors.
-# this applies 32 convolution filters of size 3x3 each.
 model.add(Conv2D(32, (3, 3), activation='relu', input_shape= input_shape))
-model.add(Conv2D(32, (3, 3), activation='relu'))
-#model.add(Conv2D(32, (3, 3), activation='relu', input_shape=x_train.shape[1:]))
-#model.add(Conv2D(32, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
-
 model.add(Conv2D(64, (3, 3), activation='relu'))
-#model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
-
 model.add(Flatten())
 model.add(Dense(256, activation='relu'))
 model.add(Dropout(0.5))
